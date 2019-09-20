3f7dc446417a0764a44b79e328618e0a011d3188
TooslaJo <pp1738qq38@gmail.com>
2019-09-21 02:16:07 +0800

    0921 UPDATE 1

--- js/web.js
+++ js/web.js
@@ -16,16 +16,16 @@ $(document).ready(function(){
     //點選nav任一項時，移動至相對應div
     $("nav>div:nth-child(1)").click(
         function(){
-            $("html,body").animate({"scrollTop":$("#TWO").offset().top-30});
+            $("html,body").animate({"scrollTop":$("#TWO").offset().top-60});
     });
     $("nav>div:nth-child(2)").click(
         function(){
-            $("html,body").animate({"scrollTop":$("#THREE").offset().top-30});
+            $("html,body").animate({"scrollTop":$("#THREE").offset().top-60});
     });
     
     $("nav>div:nth-child(3)").click(
         function(){
-            $("html,body").animate({"scrollTop":$("#FOUR").offset().top-30});
+            $("html,body").animate({"scrollTop":$("#FOUR").offset().top-60});
     });
     $("nav>div:nth-child(4)").click(
         function(){
@@ -38,19 +38,7 @@ $(document).ready(function(){
             $("html,body").animate({"scrollTop":"0"});
             n=1;
     });
-    /*
-    var pic = $("#PIC")
-    pic.hover(
-        function(){
-            $(this).css({"background-color":"gold",
-                        "color":"whitesomke",
-                        "transition": "all 0.2s ease"});
-        },function(){
-            $(this).css({"background-color":"green",
-                        "color":"black",
-                        "transition": "all 0.2s ease"
-    });*/
-    
+
     $(window).load(function() {
         $('.flexslider').flexslider({
             animation: "slide", 
@@ -62,44 +50,17 @@ $(document).ready(function(){
         });
     });
 
-    /*$("#ONE,#TWO,#THREE,#FOUR,#FIVE").hover(
-        function(){
-            $(this).css("background-color","#eed9d4");
-            $(this).css("transition","all 0.2s ease");
-        }
-        ,function(){
-            $(this).css("background-color","#eeb7b2");
-            $(this).css("transition","all 0.2s ease");
-    });*/
+    //內容方塊先消失
+    $("#ONE,#TWO,#THREE,#FOUR,#FIVE").hide(0);
+    $("#ONE,#TWO,#THREE,#FOUR,#FIVE").fadeIn(2000);
 
     //回首頁鈕先消失
     $("#gotop").fadeOut(0);
-    //內容方塊先消失
-    $("#ONE").hide(0);
-    //$("#ONE,#TWO,#THREE,#FOUR,#FIVE").hide(0);
-    $("#ONE").fadeIn(2000);
-
-    //回首頁鈕判斷機制
     $(window).scroll(
         function(){
             if($(window).scrollTop()>=$("article div").offset().top){
-                $("#gotop").fadeIn();
+                $("#gotop").fadeIn(500);
             }else{
-                $("#gotop").fadeOut();
-            }
-            if($(window).scrollTop()>=$("nav").offset().top){
-                $("#TWO").fadeIn(1000);
-                if($(window).scrollTop()>=$("#ONE").offset().top){
-                    $("#THREE").fadeIn(1000);
-                    if($(window).scrollTop()>=$("#TWO").offset().top){
-                        $("#FOUR").fadeIn(1000);
-                        if($(window).scrollTop()>=$("#THREE").offset().top){
-                            $("#FIVE").fadeIn(1000);
-                        }
-                    }
-                }            
-            }
-        }
-    )
-
+                $("#gotop").fadeOut(500);}
+        });
 });
